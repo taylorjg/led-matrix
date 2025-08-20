b43d24010b5f4ea085d95ccc0cd971be8c9b0024
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2025-08-20 01:03:35 +0100

    Deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/led-matrix",
   plugins: [react()],
   resolve: {
     // eslint-disable-next-line no-undef
